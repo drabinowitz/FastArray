269f0a18c781f1d9e1b9616516029d319dbf897b
drabinowitz <dmitrirabinowitz+development@gmail.com>
2014-11-02 21:54:37 -0500

    created testing framework

---
+++ spec/FastArray.js
@@ -0,0 +1,20 @@
+/* global FastArray, describe, it, expect, should */
+
+describe('FastArray()', function () {
+  'use strict';
+
+  it('exists', function () {
+    expect(FastArray).to.be.a('function');
+
+  });
+
+  it('does something', function () {
+    expect(true).to.equal(false);
+  });
+
+  it('does something else', function () {
+    expect(true).to.equal(false);
+  });
+
+  // Add more assertions here
+});
